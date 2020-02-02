1855592185f97316cc300508a3eabc8b3db9d0c7
Ismael Cabañas García <ismaelcabanas@gmail.com>
2020-02-02 17:05:59 +0100

    Delete all repo files

--- src/main/java/com/grupocmc/protein/mvc/exceptions/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.grupocmc.protein.mvc.exceptions;
-
-public class NotFoundException  extends RuntimeException {
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/grupocmc/protein/security/SecurityFilter.java
+++
@@ -1,9 +0,0 @@
-package com.grupocmc.protein.security;
-
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-@Component
-public class SecurityFilter extends HandlerInterceptorAdapter{
-
-}
